2f334cdb0afbe61d4dd34dc7f0b0fe1b4d1996ea
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-05-05 23:50:52 -0500

    Initial commit

---
+++ demoConnection.js
@@ -0,0 +1,21 @@
+// Demo MySQL database connection (https://www.w3schools.com/nodejs/nodejs_mysql.asp)
+
+// what is require
+var mysql = require('mysql2');
+
+// how does it know .createConnection?
+// what other params can i give?
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "duncan",
+    password: "citsdygo"
+});
+
+// how does it know .connect?
+// what is throw?
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// run with node demoConnection.js
